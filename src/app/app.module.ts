d3968470ea2ae2a24b6b4846a31104d817e836cc
razvantomegea <razvan.tomegea@gmail.com>
2016-12-22 13:56:00 +0200

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+
 import { thirdPartyImports } from './config/imports/third-party'
 import { AppComponent } from './app.component';
 import { thgDeclarations, thgImports, thgProviders } from './config/imports/thg';
@@ -8,8 +8,7 @@ import { thgDeclarations, thgImports, thgProviders } from './config/imports/thg'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ...thgDeclarations
+    
   ],
   imports: [
     BrowserModule,
--- src/app/config/imports/thg.ts
+++ src/app/config/imports/thg.ts
@@ -6,7 +6,6 @@ import { AuthComponent } from '../../auth/auth.component';
 import { AuthGuard } from '../../auth/auth-guard.service';
 import { AuthService } from '../../auth/auth.service';
 import { FitnessModule } from '../../fitness/fitness.module';
-import { HelperService } from '../../shared/helper.service';
 import { HomeComponent } from '../../home/home.component';
 import { NutritionModule } from '../../nutrition/nutrition.module';
 
@@ -26,6 +25,5 @@ export const thgProviders = [
     AccountEditResolve,
     AccountEditService,
     AuthGuard,
-    AuthService,
-    HelperService
+    AuthService
 ];
