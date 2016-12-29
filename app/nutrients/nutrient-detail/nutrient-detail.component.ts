95d5c7e7dd59ce672f1d5ac313cfa99a1ca103de
Razvan Tomegea <razvan.tomegea@gmail.com>
2016-12-29 13:41:15 +0200

    added full nutrient description

--- app/nutrients/nutrient-detail/nutrient-detail.component.ts
+++ app/nutrients/nutrient-detail/nutrient-detail.component.ts
@@ -13,6 +13,9 @@ import { Nutrient } from '../shared/nutrient.model';
 })
 export class NutrientDetailComponent implements OnInit {
   public nutrient: Nutrient;
+  public nutrientClassification: string = '';
+  public nutrientDiseases: string = '';
+  public nutrientFunctions: string = '';
   constructor(
     private changeDetectionRef: ChangeDetectorRef,
     private route: ActivatedRoute,
@@ -26,6 +29,21 @@ export class NutrientDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.data.subscribe((data: { nutrient: Nutrient }) => {
       this.nutrient = data.nutrient;
+      if (this.nutrient.hasOwnProperty('classification')) {
+        this.nutrient.classification.forEach((item: any) => {
+          this.nutrientClassification += `<h3>${item.name}</h3><p>${item.description}</p>`;
+        });
+      } else {
+        this.nutrientClassification = 'There are no major classifications for this nutrient';
+      }
+      this.nutrient.diseasePrev.forEach((item: string) => {
+        this.nutrientDiseases += `&#8226; ${item}<br/>`;
+      });
+
+      this.nutrient.functions.forEach((item: string) => {
+        this.nutrientFunctions += `&#8226; ${item}<br/>`;
+      });
+
       this.changeDetectionRef.detectChanges();
     });
   }
